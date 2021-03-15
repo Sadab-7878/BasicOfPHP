f1c03e1214fe204538695fb0635a9a924f86a338
Sadab-7878 <57608969+Sadab-7878@users.noreply.github.com>
2021-03-16 02:42:05 +0600

    Add files via upload

---
+++ 01_Basic.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+    
+  
+
+    <?php
+
+        //constaints
+        define('PI',3.14);
+            // echo "Hello World ";
+
+            // //echo "<br>";
+
+            //  $var1 = 31;
+            //  $var2 = 32;
+
+            // echo $var1 ;
+            // echo "<br>";
+            // echo $var2;
+
+            // echo $var1+$var2;
+
+
+            // echo "The Value of 1>=4 is ";
+            // echo var_dump(1>=4);
+
+
+         // echo $var1++;
+         // echo "<br>";
+         // echo $var1;
+        //    echo $var2++;
+        //    echo $var1--;
+        //    echo $var2--;
+
+        
+        // $my_var = (true) and (true);
+        // echo var_dump($my_var);
+
+
+        //data types
+
+        // $v1 = "This is Sadab";
+        // echo var_dump($v1);
+
+        // echo "<br>";
+
+        // $v2 = true;
+        // echo var_dump($v2);
+
+
+       // echo PI;
+
+       
+        
+
+
+
+    ?>
+    
+    </div>
+
+
+
+</body>
+</html>
---
+++ 02_More_basic.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+
+
+<style>
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border box;
+    }
+
+    .contain{
+
+        max-width:80%;
+        background-color: rgb(214, 148, 148);
+        margin: auto;
+        padding: 23px;
+    }
+
+
+
+
+</style>
+<body>
+    
+    <div class="contain">
+
+        <h1>Let's Learn PHP</h1>
+        
+
+
+       
+    <?php
+
+        // $age = 34;
+
+        // if($age>18){
+        //     echo "You can go to the party";
+        // }
+        // else{
+        //     echo "You can not go the party";
+        // }
+
+
+        // $languages = array("Python","C++","php","NodeJs");
+        // echo $languages[0];
+        // echo "<br>";
+
+        // //number of array items to show
+        
+        // echo count($languages);
+
+
+
+        // $a = 0;
+        // while ($a <= 10) {
+        //    echo "<br>The value of a is: ";
+        //    echo $a;
+        //    $a++;
+        // }
+
+
+        
+        // $a = 0;
+        // while ($a < count($languages)) {
+        //    echo "<br>The value of languages is: ";
+        //    echo $languages[$a];
+        //    $a++;
+        // }
+
+
+        // foreach($languages as $value){
+        //     echo "<br> The value is ";
+            
+        //     echo $value;
+        // }
+
+
+        // function print5(){
+        //     echo "FIVE";
+        // }
+
+
+        // print5();
+
+
+        // function print_number($number){
+        //     echo "<br>Your Number is ";
+        //     echo $number;
+        // }
+
+        // print_number(45);
+        // print_number(653);
+
+
+    ?>
+    
+    
+    </div>
+
+
+
+</body>
+</html>
